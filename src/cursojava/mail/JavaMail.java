86e90a7d9c08a2902df0ddb6d9b5320b46949b3d
Fabio <ftakematsu@gmail.com>
2021-10-22 18:51:42 -0300

    Criando classe pessoa

---
+++ src/cursojava/mail/JavaMail.java
@@ -0,0 +1,10 @@
+package cursojava.mail;
+
+public class JavaMail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/cursojava/mail/Pessoa.java
@@ -0,0 +1,6 @@
+package cursojava.mail;
+
+public class Pessoa {
+	String nome;
+	int idade;
+}
